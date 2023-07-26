a580fd896abbb1ddff86155c89caaed0962f7a87
T1 <126980610+tadash10@users.noreply.github.com>
2023-07-26 11:28:28 -0300

    Create security_events.py

---
+++ security_events.py
@@ -0,0 +1,7 @@
+def handle_privilege_escalation_event(event):
+    # Logic to handle privilege escalation event
+    print("Privilege Escalation Event:", event.message)
+
+def handle_suspicious_container_event(event):
+    # Logic to handle suspicious container behavior event
+    print("Suspicious Container Event:", event.message)
